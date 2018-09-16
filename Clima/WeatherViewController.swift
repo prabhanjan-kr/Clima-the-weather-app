694d99e7a75cc6263c1780cfb9bda5c7374e93cd
prabhanjan-kr <prabhanjankr@protonmail.com>
2018-09-16 18:11:39 +0530

    App ID changed from openweather map api

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -13,7 +13,7 @@ class WeatherViewController: UIViewController {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "8a47bb43eb2e4587a45f5f0db37ce32e"
     
 
     //TODO: Declare instance variables here
